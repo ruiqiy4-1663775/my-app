2debae152b4beea8e5b7559624c089162609814b
ruiqiy4-1663775 <732023303@qq.com>
2023-07-31 13:35:13 -0700

    ok

--- src/form.js
+++ src/form.js
@@ -2,10 +2,10 @@ function Form() {
 
     return (
         <form className="border border-white grid grid-cols-1 lg:grid-cols-3 container gap-x-20">
-            <div className="border border-white text-center p-3">hello</div>
-            <div className="border border-white text-center p-3">hello</div>
-            <div className="border border-white text-center p-3">hello</div>
-            <div className="border border-white text-center p-3">hello</div>
+            <div className="border border-white px-10 py-3">hello</div>
+            <div className="border border-white px-10 py-3">hello</div>
+            <div className="border border-white px-10 py-3">hello</div>
+            <div className="border border-white px-10 py-3">hello</div>
         </form>
     )
 }
