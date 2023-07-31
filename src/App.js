97799dcdd0b3ce761e70e0441d8738d37c61ac81
ruiqiy4-1663775 <732023303@qq.com>
2023-07-31 12:59:25 -0700

    responsive grid.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import FileUpload from './fileUpload';
 import { Login } from './login';
 import {priceUpdate, priceUpdateMolding} from './utility';
+import Form from './form';
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Login/>
       <FileUpload priceUpdate={priceUpdate} title={'Special price update'} indexArray={[1, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18]}/>
       <FileUpload priceUpdate={priceUpdateMolding} title={'Special price update Molding'} indexArray={[1, 6, 7, 8, 9, 10]}/>
+      <Form/>
     </div>
   );
 }
---
+++ src/form.js
@@ -0,0 +1,13 @@
+function Form() {
+
+    return (
+        <form className="border border-white grid grid-cols-1 lg:grid-cols-3 container gap-x-20">
+            <div className="border border-white text-center p-3">hello</div>
+            <div className="border border-white text-center p-3">hello</div>
+            <div className="border border-white text-center p-3">hello</div>
+            <div className="border border-white text-center p-3">hello</div>
+        </form>
+    )
+}
+
+export default Form;
