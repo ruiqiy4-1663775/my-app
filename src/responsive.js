a3f8541c2499c5234d778ee644ff8cf8f4dbb9b3
ruiqiy4-1663775 <732023303@qq.com>
2023-08-01 12:46:38 -0700

    ok

---
+++ src/responsive.js
@@ -0,0 +1,50 @@
+function Res() {
+    return (
+        <div className="overflow-x-auto">
+            <div className="table min-w-max">
+                <div className="table-header-group ">
+                    <div className="table-row"> {/* Add a onclick here to open a modal */}
+                        <div className="table-cell text-left ">Song</div>
+                        <div className="table-cell text-left ">Artist</div>
+                        <div className="table-cell text-left ">Year</div>
+                        <div className="table-cell text-left ">Year</div>
+                        <div className="table-cell text-left ">Year</div>
+                        <div className="table-cell text-left ">Year</div>
+                        <div className="table-cell text-left ">Year</div>
+                    </div>
+                </div>
+                <div className="table-row-group">
+                    <div className="table-row">
+                        <div className="table-cell ">The Sliding Mr. Bones (Next Stop, Pottersville)</div>
+                        <div className="table-cell ">The Sliding Mr. Bones (Next Stop, Pottersville)</div>
+                        <div className="table-cell ">The Sliding Mr. Bones (Next Stop, Pottersville)</div>
+                        <div className="table-cell text-left ">The Sliding Mr. Bones (Next Stop, Pottersville)</div>
+                        <div className="table-cell text-left ">The Sliding Mr. Bones (Next Stop, Pottersville)</div>
+                        <div className="table-cell text-left ">The Sliding Mr. Bones (Next Stop, Pottersville)</div>
+                        <div className="table-cell text-left ">The Sliding Mr. Bones (Next Stop, Pottersville)</div>
+                    </div>
+                    <div className="table-row">
+                        <div className="table-cell ">Witchy Woman</div>
+                        <div className="table-cell ">The Eagles</div>
+                        <div className="table-cell ">1972</div>
+                        <div className="table-cell text-left ">Year</div>
+                        <div className="table-cell text-left ">Year</div>
+                        <div className="table-cell text-left ">Year</div>
+                        <div className="table-cell text-left ">Year</div>
+                    </div>
+                    <div className="table-row min-w-fit">
+                        <div className="table-cell ">Shining Star</div>
+                        <div className="table-cell ">Earth, Wind, and Fire</div>
+                        <div className="table-cell ">1975</div>
+                        <div className="table-cell text-left ">Year</div>
+                        <div className="table-cell text-left ">Year</div>
+                        <div className="table-cell text-left ">Year</div>
+                        <div className="table-cell text-left ">Year</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Res;
